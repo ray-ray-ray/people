1168da3c7bc04ebd941b214bd1ef09ecd9ec6977
RAY <ray.allen.courtney@gmail.com>
2014-08-03 15:40:51 -0700

    Set up nosetests structure. Fixes #5

---
+++ test/unit/service/test_person.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the Person service.
+"""
+from unittest import TestCase
+
+__author__ = 'RAY'
+
+
+class TestCreatePerson(TestCase):
+    def test_create_person(self):
+        self.fail()
